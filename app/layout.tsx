b70e7f64c5140b240318e2f03ea89137285d5dc1
J-J-Day <Jacobday27@hotmail.com>
2026-02-15 19:38:43 +0000

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "APEX Portal",
+  description: "APEX Grant Solutions Portal"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* Inter */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        {/* Font Awesome */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
