8cc587b87d35e4853236ca1ba07e17463996ece4
J-J-Day <Jacobday27@hotmail.com>
2026-02-17 20:46:36 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,25 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
+
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "APEX Portal",
-  description: "APEX Grant Solutions Portal"
+  description: "APEX Grant Solutions Portal",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <head>
-        {/* Inter */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-  
-        {/* Font Awesome */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        />
-      </head>
-      <body className={`${inter.className} antialiased`}>
-  {children}
-</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
